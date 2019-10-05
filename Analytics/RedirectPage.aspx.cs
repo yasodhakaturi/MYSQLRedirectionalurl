36e38e4609638c93ebe5f223da350ee173d33a6e
yasodha <yasodha.bitra@gmail.com>
2019-10-04 23:09:48 -0700

    adding code for back button

--- Analytics/RedirectPage.aspx.cs
+++ Analytics/RedirectPage.aspx.cs
@@ -10,6 +10,12 @@ namespace Analytics
     public partial class RedirectPage : System.Web.UI.Page
     {
         shortenurlEntities dc = new shortenurlEntities();
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.Now.AddSeconds(-1));
+            Response.Cache.SetNoStore();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
